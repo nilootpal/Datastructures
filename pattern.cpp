da233481680fdfa1e376532faf3d3bd0d0fc615d
JrOiKsEiR <nilootpaldas@gmail.com>
2022-05-23 14:35:28 +0530

    patterncomplete

--- pattern.cpp
+++ pattern.cpp
@@ -2,7 +2,71 @@
 using namespace std;
 
 int main(){
-    
+    //zig-zag pattern
+    for( int i = 1; i <= 3; ++i ){
+        for( int j = 1; j <= 9; ++j ){
+            if( ((i + j)%4 == 0) || (i == 2) && ((j%4) == 0) ){
+                cout<<" * ";
+            }
+            else{
+                cout<<"   ";
+            }
+
+        }
+        cout<<endl;
+    }
+
+    //palindromic pattern
+    // for(int i = 1; i <= 5; ++i){
+    //     for(int j = 1; j <= 5-i; ++j){
+    //         cout<<"   ";
+    //     }
+    //     int k = i;
+    //     for(int j = 1; j <= i; ++j){
+    //         cout<<k--<<"  ";
+    //     }
+    //     k = 2;
+    //     for(int j = 1; j < i; ++j){
+    //         cout<<k++<<"  ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //number pattern
+    // for(int i = 1; i <= 5; ++i){
+    //     for(int j = 1; j <= 5-i; ++j){
+    //         cout<<"  ";
+    //     }
+    //     for(int j = 1; j <= i; ++j){
+    //         cout<<"  "<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //rhombus pattern
+    // for(int i = 1; i <= 5; ++i){
+    //     for(int j = 1; j <= 5-i; ++j){
+    //         cout<<" ";
+    //     }
+    //     for(int j = 1; j <= 5; ++j){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //01 pattern
+    // for(int i = 1; i <= 5; ++i){
+    //     for(int j = 1; j <= i; ++j){
+    //         if((i+j)%2){
+    //             cout<<" 0 ";
+    //         }
+    //         else{
+    //             cout<<" 1 ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
     // floyd triangle
     // int count = 1;
 
