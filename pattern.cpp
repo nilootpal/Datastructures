480fdc79fb84d236c5882431643f3e56d48c7ae0
JrOiKsEiR <nilootpaldas@gmail.com>
2022-05-23 13:50:35 +0530

    first commit

---
+++ pattern.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // floyd triangle
+    // int count = 1;
+
+    // for(int i = 1; i <= 5; ++i){
+    //     for ( int j = 5-i; j < 5; j++ ){
+    //         cout<<"   "<<count++<<"     ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //half pyramid after 180deg rotation
+    // for( int i = 0; i < 5; ++i ){
+    //     for( int j = 0; j < 5; ++j ){
+    //         if(j < 5-i){
+    //             cout<<"   ";
+    //         }
+    //         else{
+    //             cout<<" * ";
+    //         }
+    //     }
+    //     // for( int j = 0; j < 5-i; ++j ){
+    //     //     cout<<"   ";
+    //     // }
+    //     // for( int j = 0; j <= i; ++j ){
+    //     //     cout<<" * ";
+    //     // }
+    //     cout<<endl;
+    // }
+
+
+
+//hollow rectangle
+    // for( int i = 0; i < 5; ++i ){
+    //     for( int j = 0; j < 4; ++j ){
+    //         if( i == 0 || i == 4 ){
+    //             cout<<" * ";
+    //         }
+    //         else if( j == 0 || j == 3 ){
+    //             cout<<" * ";
+    //         }
+    //         else{
+    //             cout<<"   ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    return 0;
+}
