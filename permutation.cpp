ea85363ab347cb130a1f8d142a08dbc756ee39ef
JrOiKsEiR <nilootpaldas@gmail.com>
2022-05-29 20:09:47 +0530

    backtracking

--- permutation.cpp
+++ permutation.cpp
@@ -6,12 +6,15 @@ using namespace std;
 vector<vector<int>> ans;
 
 void permute(vector<int> &a, int idx){
-    if(idx == a.length()){
-        ans.push_back(idx);
+    if(idx == a.size()){
+        ans.push_back(a);
         return;
     }
 
-    for(int i = idx; i < s.length(); ++i){
+    for(int i = idx; i < a.size(); ++i){
+        if((a[i] == a[idx]) && (i != idx)){
+            continue;
+        }
         std::swap(a[i], a[idx]);
         permute(a, idx+1);
         std::swap(a[i], a[idx]);
@@ -30,7 +33,7 @@ int main(){
         cin>>x;
     }
 
-    permute(a);
+    permute(a, 0);
 
     for( auto v : ans ){
         for( auto i : v){
